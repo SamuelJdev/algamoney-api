3ea8524155ce3fe22cb9c5c1356d72536e1db2ed
Samuel <samuel.encon@gmail.com>
2022-10-13 23:13:16 -0400

    lombok com problemas, removido

--- algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,13 +16,28 @@ import lombok.Setter;
 @Entity
 @Table(name = "categoria")
 public class Categoria {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
-	@NotNull
 	private String nome;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	@Override
 	public int hashCode() {
@@ -41,4 +55,9 @@ public class Categoria {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(codigo, other.codigo);
 	}
+
+	@Override
+	public String toString() {
+		return "Categorias [codigo=" + codigo + ", nome=" + nome + "]";
+	}	
 }
